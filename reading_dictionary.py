a0b70c195cdef8327b9e3d8374a6c67b8a10ef9a
Chais Dean <capslock@Chaiss-MacBook-Pro.local>
2017-07-09 20:27:33 -0700

    read diction

---
+++ reading_dictionary.py
@@ -0,0 +1,26 @@
+# Assignment: Making and Reading from Dictionaries
+# Create a dictionary containing some information about yourself. The keys should include name, age, country of birth, favorite language.
+#
+# Write a function that will print something like the following as it executes:
+#
+# My name is Anna
+# My age is 101
+# My country of birth is The United States
+# My favorite language is Python
+# Copy
+# There are two steps to this process, building a dictionary and then gathering all the data from it. Write a function that can take in and print out any dictionary keys and values.
+#
+# Note: The majority of data we will manipulate as web developers will be hashed in a dictionary using key-value pairs. Repeat this assignment a few times to really get the hang of unpacking dictionaries, as it's a very common requirement of any web application.
+
+
+def reading_dictionary():
+    name = {
+    "name":"Anna",
+    "age":101,
+    "country":"The United States",
+    "language":"Python"
+    }
+    for key,data in name.iteritems():
+        print "My", key,"is", data
+
+print reading_dictionary()
